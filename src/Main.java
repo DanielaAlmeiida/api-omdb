45f68a976469d7a0ef1810d528be4519456cc5db
Daniela Almeida <danielaspbrasil@gmail.com>
2023-07-25 17:48:08 -0300

    Adicionado função de conversão para json

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,87 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import br.com.alura.screenmatch.exception.YearConversionErrorException;
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner read = new Scanner(System.in);
+        String search = "";
+        List<Titulo> titulos = new ArrayList<>();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Gson gson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+            .setPrettyPrinting()
+            .create();
+
+        while (!search.equalsIgnoreCase("exit")) {
+                System.out.println("Digite um filme para busca: ");
+                search = read.nextLine();
+
+                if(search.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                String address = "http://www.omdbapi.com/?t=" + search.replace(" ", "+") + "&apikey=7a4a0d89";
+
+
+                try {
+                    HttpClient client = HttpClient.newHttpClient();
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create(address))
+                            .build();
+
+                    HttpResponse<String> response = client
+                            .send(request, HttpResponse.BodyHandlers.ofString());
+
+                    String json = response.body();
+                    System.out.println(json);
+
+
+                    TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+
+
+                    System.out.println(meuTituloOmdb);
+
+
+                    Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                    System.out.println("Titulo já convertido");
+                    System.out.println(meuTitulo);
+
+                   titulos.add(meuTitulo);
+                } catch (NumberFormatException e) {
+                    System.out.println("Aconteceu um erro: ");
+                    System.out.println(e.getMessage());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Aconteceu um erro: :" + e.getMessage());
+
+                } catch (YearConversionErrorException e) {
+                    System.out.println("Aconteceu um erro: :" + e.getMessage());
+                } catch (Exception e) {
+                    System.out.println("Aconteceu um erro: :" + e.getMessage());
+                }
         }
+
+        System.out.println("Titles List --> " + titulos);
+
+        FileWriter writer = new FileWriter("movies.json");
+        writer.write(gson.toJson(titulos));
+        writer.close();
+
+        System.out.println("O programa finalizou corretamente!");
     }
 }
+
--- src/br/com/alura/screenmatch/exception/YearConversionErrorException.java
+++ src/br/com/alura/screenmatch/exception/YearConversionErrorException.java
@@ -1,2 +1,15 @@
-package br.com.alura.screenmatch.exception;public class YearConversionErrorException {
+package br.com.alura.screenmatch.exception;
+
+public class YearConversionErrorException extends RuntimeException {
+
+    private String message;
+
+    public YearConversionErrorException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
 }
